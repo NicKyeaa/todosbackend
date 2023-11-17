e538350e4cf18cac8c08597f3d6b11ac2aae0bb6
NicKyeaa <tin.jurcevic@gmail.com>
2023-11-17 23:36:37 +0100

    Added callback function for a port

--- index.js
+++ index.js
@@ -54,7 +54,9 @@ fastify.delete('/todos/:toDoID', async (req, res) => {
 
 // Run the server!
 try {
-  await fastify.listen({ port: 3500 }, 'Listening on a port 3500');
+  await fastify.listen({ port: 3500 }, () => {
+    console.log('Listening on port 3500');
+  });
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
